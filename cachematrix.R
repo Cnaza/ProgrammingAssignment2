0852c7f32c7bf35cce6ee096957d47cbc70325ef
CarlaNazario <carla.nazario@present-technologies.com>
2015-09-25 23:07:02 +0100

    commit changes

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+##call
+## > source("cachematrix.R")            load R program
+## > a <- makeCacheMatrix()             create functions
+## > a$set(matrix(c(1,2,3,4), 2, 2))    set any square invertible matrix
+## > cacheSolve(a)                      get inverse (new or in cash)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    mtxCache <- NULL
+    set <- function(y) {
+        x <<- y
+        mtxCache <<- NULL
+    }
+    get <- function() x
+    setmatrix <- function(a) mtxCache <<- a
+    getmatrix <- function() mtxCache
+    
+    list(set = set, get = get,
+         setmatrix = setmatrix,
+         getmatrix = getmatrix)
 }
 
-
-## Write a short comment describing this function
+## Return Inverse matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    mt <- x$getmatrix()
+    ##cash
+    if(!is.null(mt)) {
+        return(mt)
+    }else{
+    #new
+    data <- x$get()
+    mt <- solve(data)
+    #set matrix to inverse to cash
+    x$setmatrix(mt)
+    mt
+}
 }
